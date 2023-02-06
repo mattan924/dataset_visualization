b3bd34cc5722126666180e6cd747b26f53823814
rmatsumoto <R.Matsumoto924@gmail.com>
2023-02-06 18:54:46 +0900

    READMEへの追記

--- src/main.py
+++ src/main.py
@@ -4,6 +4,26 @@ from util import *
 from solve import solve_near_edge
 
 
+index_file = "../dataset/data/index/test.csv"
+config_file = "../dataset/config/test.csv"
+traking_file = "../dataset/data/traking/test.csv"
+assign_file = "../dataset/data/assign/test.csv"
+edge_file = "../dataset/data/edge/test.csv"
+topic_file = "../dataset/data/topic/test.csv"
+animation_file = "../dataset/animation/assign_traking.gif"
+
+generate_traking(index_file, config_file, traking_file)
+
+generate_edge(index_file, config_file, edge_file)
+
+generate_topic(index_file, config_file, topic_file)
+
+assignTopic(index_file, assign_file)
+
+create_assign_animation(index_file, animation_file, 20)
+
+
+"""
 index_file = "../../reinforcement/dataset/learning_data/index/index.csv"
 config_file = "../../reinforcement/dataset/learning_data/config/config.csv"
 traking_file = "../../reinforcement/dataset/learning_data/traking/traking.csv"
@@ -22,3 +42,4 @@ generate_topic(index_file, config_file, topic_file)
 assignTopic(index_file, assign_file)
 
 create_assign_animation(index_file, animation_file, 20)
+"""
