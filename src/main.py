6402dacfb0edaf13f36c5cb77d0fe0f13be8b4a8
rmatsumoto <R.Matsumoto924@gmail.com>
2023-04-10 15:21:07 +0900

    改修

--- src/main.py
+++ src/main.py
@@ -4,42 +4,20 @@ from util import *
 from solve import solve_near_edge
 
 
-index_file = "../../reinforcement_learning/dataset/learning_data/index/index_single2.csv"
-config_file = "../../reinforcement_learning/dataset/learning_data/config/config_single.csv"
-traking_file = "../../reinforcement_learning/dataset/learning_data/traking/traking_single2.csv"
-assign_file = "../../reinforcement_learning/dataset/learning_data/assign/assign_single2.csv"
-edge_file = "../../reinforcement_learning/dataset/learning_data/edge/edge_single2.csv"
-topic_file = "../../reinforcement_learning/dataset/learning_data/topic/topic_single2.csv"
-animation_file = "../../reinforcement_learning/dataset/learning_data/animation/animation_single2.gif"
+index_file = "../dataset/data/index/readme.csv"
+config_file = "../dataset/config/readme_config.csv"
+traking_file = "../dataset/data/traking/readme_traking.csv"
+assign_file = "../dataset/data/assign/readme_assgin.csv"
+edge_file = "../dataset/data/edge/readme_edge.csv"
+topic_file = "../dataset/data/topic/readme_edge.csv"
+animation_file = "../dataset/animation/readme_animation.gif"
 
-generate_traking(index_file, config_file, traking_file)
+generate_traking(index_file, config_file, traking_file, seed=41176121)
 
-generate_edge(index_file, config_file, edge_file)
+#generate_edge(index_file, config_file, edge_file)
 
 generate_topic(index_file, config_file, topic_file)
 
 assignTopic(index_file, assign_file)
 
 create_single_topic_animation(index_file, animation_file, 20)
-
-
-"""
-index_file = "../../reinforcement/dataset/learning_data/index/index.csv"
-config_file = "../../reinforcement/dataset/learning_data/config/config.csv"
-traking_file = "../../reinforcement/dataset/learning_data/traking/traking.csv"
-assign_file = "../../reinforcement/dataset/learning_data/assign/assign.csv"
-edge_file = "../../reinforcement/dataset/learning_data/edge/edge.csv"
-topic_file = "../../reinforcement/dataset/learning_data/topic/topic.csv"
-animation_file = "../../reinforcement/dataset/learning_data/animation/animation.gif"
-
-
-generate_traking(index_file, config_file, traking_file)
-
-generate_edge(index_file, config_file, edge_file)
-
-generate_topic(index_file, config_file, topic_file)
-
-assignTopic(index_file, assign_file)
-
-create_assign_animation(index_file, animation_file, 20)
-"""
