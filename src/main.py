5bf52b88327bf551e379128dd314e39f51bc3423
Ryota <rmatsumoto@fine.cs.kobe-u.ac.jp>
2023-05-02 14:46:58 +0900

    変更

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from util import *
 from solve import solve_near_edge
 
 
-def create_data_set(data_set_size, index_file_base, config_file, traking_file_base, assign_file_base, edge_file_base, topic_file_base):
+def create_data_set(data_set_size, index_file_base, config_file, traking_file_base, assign_file_base, edge_file_base, topic_file_base, animation_file_base=None):
     for i in range(data_set_size):
         index_file = index_file_base + "_" + str(i) + ".csv"
         traking_file = traking_file_base + "_" + str(i) + ".csv"
@@ -20,16 +20,21 @@ def create_data_set(data_set_size, index_file_base, config_file, traking_file_ba
 
         assign_topic(index_file, assign_file)
 
+        if animation_file_base != None:
+            animation_file = animation_file_base + "_" + str(i) + ".gif"
+            create_topic_animation(index_file, animation_file, 20)
+
         print(f"data_set is created {(i / data_set_size)*100}%")
 
 
-index_file_base = "../../reinforcement_learning/dataset/pretrain_data_set/index/index"
-config_file = "../../reinforcement_learning/dataset/pretrain_data_set/config/config.csv"
-traking_file_base = "../../reinforcement_learning/dataset/pretrain_data_set/traking/traking"
-assign_file_base = "../../reinforcement_learning/dataset/pretrain_data_set/assign/assign"
-edge_file_base = "../../reinforcement_learning/dataset/pretrain_data_set/edge/edge"
-topic_file_base = "../../reinforcement_learning/dataset/pretrain_data_set/topic/topic"
+index_file_base = "../../reinforcement_learning/dataset/test_data_set/index/index"
+config_file = "../../reinforcement_learning/dataset/test_data_set/config/config.csv"
+traking_file_base = "../../reinforcement_learning/dataset/test_data_set/traking/traking"
+assign_file_base = "../../reinforcement_learning/dataset/test_data_set/assign/assign"
+edge_file_base = "../../reinforcement_learning/dataset/test_data_set/edge/edge"
+topic_file_base = "../../reinforcement_learning/dataset/test_data_set/topic/topic"
+animation_file_base = "../../reinforcement_learning/dataset/test_data_set/animation/animation"
 
-dataset_size = 100
+dataset_size = 10
 
-create_data_set(dataset_size, index_file_base, config_file, traking_file_base, assign_file_base, edge_file_base, topic_file_base)
+create_data_set(dataset_size, index_file_base, config_file, traking_file_base, assign_file_base, edge_file_base, topic_file_base, animation_file_base)
